7ec4f21c66b367711e439aaacc89d082d86033b9
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2023-10-22 10:21:44 +0000

    fixed linethrough and icons socials

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,47 +10,56 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="flex bg-[#e8fdf5] ">
-      <div className="flex-1 relative">
-      <h1 className="text-2xl font-extrabold relative z-10">Maxwell Duedu</h1>
-        <div className="overlay absolute inset-7 bg-[#f5c0a2] opacity-100 ml-20 h-2"></div>
-        <p className="font-thin">
-          I'm the walking <span>&lt;kode/&gt;</span>{" "}
-        </p>
-      </div>
-      <div className="flex-1">
-        <h6 className="font-thin">KIBO SCHOOL OF TECHNOLOGY</h6>
-      </div>
-      <div className="pt-5">
-        <ul>
-          <li>
-            <img src={doc} className="w-4 h-4" alt="document logo" />
-          </li>
-          <li>
-            <img src={github} className="w-4 h-4" alt="document logo" />
-          </li>
-          <li>
-            <img src={twitter} className="w-4 h-4" alt="document logo" />
-          </li>
+      <div className="container">
+        <div className="flex-1 relative">
+          <h1 className="text-2xl font-extrabold relative z-10">
+            Maxwell Duedu
+          </h1>
+          <div className="overlay bg-top absolute inset-7 bg-[#f5c0a2] opacity-100 w-40 ml-5"></div>
 
-          <li>
-            <img src={medium} className="w-4 h-4" alt="document logo" />
-          </li>
-          <li>
-            <img src={linkedin} className="w-4 h-4" alt="document logo" />
-          </li>
-        </ul>
+
+          <p className="font-thin ">
+            I'm the walking <span>&lt;kode/&gt;</span>{" "}
+          </p>
+        </div>
+        <div className="flex-1">
+          <h6 className="font-thin">KIBO SCHOOL OF TECHNOLOGY</h6>
+        </div>
+        <div className="pt-5 ">
+          <ul>
+            <li>
+              <img src={doc} className="w-4 h-4 " alt="document logo" />
+            </li>
+            <li>
+              <img src={github} className="w-4 h-4 " alt="document logo" />
+            </li>
+            <li>
+              <img src={twitter} className="w-4 h-4 " alt="document logo" />
+            </li>
+
+            <li>
+              <img src={medium} className="w-4 h-4 " alt="document logo" />
+            </li>
+            <li>
+              <img src={linkedin} className="w-4 h-4 " alt="document logo" />
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="flex-1">
-        <img
-          src={maxwell}
-          className="w-40 headshot"
-          alt="an image of Maxwell Duedu"
-        />
-      </div>
+
       <div>
-        <p className="ml-5 mr-5 pb-3">
-          "Fluent Python is the best book to learn Python"
-        </p>
+        <div className="flex-1">
+          <img
+            src={maxwell}
+            className="w-40 headshot"
+            alt="an image of Maxwell Duedu"
+          />
+        </div>
+        <div>
+          <p className="ml-5 mr-5 pb-3">
+            "Fluent Python is the best book to learn Python"
+          </p>
+        </div>
       </div>
     </div>
   );
