a240682db318a80a69bcfe53c2468d8e223c7e36
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2023-10-26 10:14:54 +0000

    added favicon

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,10 +7,10 @@ function About() {
       <h2 className="text-2xl font-extrabold ">About</h2>
       <div className="about-info">
         <p className="text-left text-pad">
-          My name is Maxwell, and I am a Software Engineer. Lorem ipsum, dolor
-          sit amet consectetur adipisicing elit. Cum, incidunt, quod ut iste
-          reprehenderit distinctio qui error animi ab dignissimos possimus neque
-          sint rerum voluptate, libero doloremque sunt quia vel?
+          Welcome to my corner of the web! I am Maxwell Duedu, a passionate and
+          innovative Software Engineer with a keen eye for solving complex
+          problems and a love for turning ideas into reality. I'm excited to
+          share my journey, skills, and projects with you.
         </p>
         <br />
         <p className="text-left text-pad">
